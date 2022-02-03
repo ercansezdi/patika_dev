e882193d9944ac771651255e0957dd4922c7b79f
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-03 17:04:49 +0300

    update
    project3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1, num2 = 5;
+
+            bool value = (num1 > num2);
+            Console.WriteLine(value);
+        }
+    }
+}
