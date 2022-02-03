1dad1d05eed27cd294d2fecdcf66cb87fd1bb9a0
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-03 16:58:49 +0300

    new
    new project

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string date = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(date);
+        }
+    }
+}
