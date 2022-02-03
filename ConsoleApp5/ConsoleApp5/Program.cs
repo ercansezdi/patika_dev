0d0b06ef78f4d37e08fe79f354ad8cae94bf621e
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-03 19:03:38 +0300

    update
    loop, if else

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i<= 100; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i.ToString() + ": Odd");
+                else
+                    Console.WriteLine(i.ToString() + ": Even");
+            }
+        }
+    }
+}
