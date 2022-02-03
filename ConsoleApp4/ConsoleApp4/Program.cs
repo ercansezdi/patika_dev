cc5ab763b3ba9b6706806b16743323655f08cf63
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-03 18:24:50 +0300

    update
    if  else

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  int_note = Int32.Parse( Console.ReadLine());
+            string note = int_note < 50 ? "FF" : int_note < 60 ? "CC" : int_note < 70 ? "CB" : int_note < 80 ? "BB" : int_note < 90 ? "BA" : int_note < 100 ? "AA" : "Wrong Note";
+
+            Console.WriteLine(note);
+        }
+    }
+}
