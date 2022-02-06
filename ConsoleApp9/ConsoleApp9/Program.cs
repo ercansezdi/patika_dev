cd3d52fb0338083994b96ace58e2d19532a6432e
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-07 01:01:01 +0300

    update
    ConsoleApp9

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 4, 3 };
+
+            Convert.ToString(test[0]).Show();
+            int ans = test.powAl();
+            Convert.ToString(ans).Show();
+
+        }
+    }
+    public static class Extansion
+    {
+        public static int powAl(this int[] nums)
+        {
+            return nums[0] * nums[1];
+        }
+        public static void Show(this string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+}
