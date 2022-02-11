6b991842ad7c92cfed41418a8a56f4040b423bc4
Ercan Sezdi <45130158+esezdi@users.noreply.github.com>
2022-02-11 04:40:07 +0300

    update
    ConsolApp10

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan test = new Calisan();
+            test.Ad = "Ercan";
+            test.Soyad = "Sezdi";
+            test.No =  160845;
+            test.Depertman = "EEE";
+
+            test.CalisanBilgileri();
+        }
+    }
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Depertman;
+
+
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı : {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı : {0}", Soyad);
+            Console.WriteLine("Çalışan Numarası : {0}", No);
+            Console.WriteLine("Çalışan Depermanı : {0}", Depertman);
+        }
+
+    }
+}
